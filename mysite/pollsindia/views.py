bad9e64c5ea7f881ea81da5863e7878c545c86bd
rafiqul0396 <rafiqulislam.mca18.du@gmail.com>
2021-06-27 13:22:59 +0530

    todays second commited

--- mysite/pollsindia/views.py
+++ mysite/pollsindia/views.py
@@ -7,4 +7,16 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 def home(request):
     return HttpResponse("I am home nw!")
+
+
+def detail(request,question_id):
+    return HttpResponse("you're looking at question %s."%question_id)
+
+
+def results(request,question_id):
+    response="you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("ypu arre voting on question %s." %question_id)
 # Create your views here.
